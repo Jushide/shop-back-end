79624a063f454e3215f3072959f0e5007756d82f
Jushide <maciuxn@gmail.com>
2022-11-18 14:22:15 +0100

    Full template

--- src/main.ts
+++ src/main.ts
@@ -1,27 +1,27 @@
-import { NestFactory, Reflector } from "@nestjs/core";
-import { ClassSerializerInterceptor, ValidationPipe } from "@nestjs/common";
-import { AppModule } from "./app.module";
-import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
+import { NestFactory, Reflector } from "@nestjs/core"
+import { ClassSerializerInterceptor, ValidationPipe } from "@nestjs/common"
+import { AppModule } from "./app.module"
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger"
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule, {
         bodyParser: false,
         //cors: true
-    });
+    })
 
-    app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
+    app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)))
 
     app.useGlobalPipes(
         new ValidationPipe({
             whitelist: true,
             transform: true,
-        })
-    );
+        }),
+    )
 
-    const options = new DocumentBuilder().setTitle("Shop API").build();
-    const document = SwaggerModule.createDocument(app, options);
-    SwaggerModule.setup("api", app, document);
+    const config = new DocumentBuilder().setTitle("Shop API").setVersion("v0.1").build()
+    const document = SwaggerModule.createDocument(app, config)
+    SwaggerModule.setup("api", app, document)
 
-    await app.listen(3000);
+    await app.listen(3000)
 }
-bootstrap();
+bootstrap()
